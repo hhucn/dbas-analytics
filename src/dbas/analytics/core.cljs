273d7aa1fa1230df5b5cba31c9638688ba6b4da8
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2017-12-21 18:35:27 +0100

    Improve live updating charts

--- src/dbas/analytics/core.cljs
+++ src/dbas/analytics/core.cljs
@@ -1,51 +1,58 @@
 (ns dbas.analytics.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [reagent.core :as r :refer [atom]]
             [secretary.core :as secretary :include-macros true]
             [accountant.core :as accountant]
             [cljs-http.client :as http]
-            [cljs.core.async :refer [chan <!]]))
+            [cljs.core.async :refer [<! timeout]]
+            [dbas.analytics.highchart :as highchart]))
 
 (def dbas-base "https://dbas.cs.uni-duesseldorf.de")
-
-(defn chart [chart-config]
-  (r/create-class {:reagent-render      (fn [_] [:div {:style {:min-width "310px" :max-width "800px"
-                                                               :height    "400px" :margin "0 auto"}}])
-                   :component-did-mount #(js/Highcharts.Chart. (r/dom-node %) (clj->js chart-config))}))
 
 (defn query-dbas [query]
   (http/get (str dbas-base "/api/v2/query?q=query{" query "}")))
 
-(defn argument-of-issues-chart []
-  (let [state (r/atom [:div]) ; holds an empty div until the fetching of the D-BAS data is done
-        refresh (fn [] (go (let [response (query-dbas "issues{title,statements{uid}}")
-                                 data (mapv #(hash-map :name (:title %)
-                                                       :y (count (:statements %)))
-                                            (get-in (<! response) [:body :issues]))]
-                                (reset! state ; set the state with the following chart. Redraws automatically
-                                        [chart {:chart       {:type "pie"}
-                                                :title       {:text "Arguments of Issues"}
-                                                :subtitle    {:text "Source: https://dbas.cs.uni-duesseldorf.de"}
+;; -------------------------
+;; Views
+
+(defn argument-of-issues-chart [{:keys [refresh-timeout]}]
+  (let [id (random-uuid)
+        data (r/atom [])]
+    (go-loop [] (let [response (query-dbas "issues{title,statements{uid}}")
+                      d (mapv #(hash-map :name (:title  %)
+                                         :y (count (:statements %)))
+                              (get-in (<! response) [:body :issues]))]
+                  (reset! data d)
+                  (when refresh-timeout
+                    (<! (timeout refresh-timeout))
+                    (recur))))
+    (fn []
+      [:div.card [highchart/chart {:chart-meta {:id id}
+                                   :chart-data {:chart       {:type "pie"}
+                                                :title       {:text "Statements of Issues"}
+                                                :subtitle    {:text (str "Source: " dbas-base)}
                                                 :plotOptions {:pie {:allowPointSelect true
                                                                     :cursor           "pointer"
                                                                     :dataLabels       {:enabled false
                                                                                        :format  "<b>{point.name}</b>: {point.y}"}
                                                                     :showInLegend     true}}
                                                 :credits     {:enabled false}
-                                                :series      [{:name         "Arguments"
+                                                :series      [{:id 0
+                                                               :name         "Statements"
                                                                :colorByPoint true
-                                                               :data data}]}]))))]
-    (refresh)
-    (fn [] ; render function. If 'state' changes, the component is reloaded
-      [:div @state])))
+                                                               :data         @data}]}}]])))
 
-;; -------------------------
-;; Views
 
 (defn home-page []
-  [:div [:h2 "Welcome to dbas-analytics"]
-   [:div [:a {:href "/about"} "go to about page"]
-    [argument-of-issues-chart]]])
+  [:div.container
+   [:div.row [:h1.col-md-12 "Welcome to dbas-analytics"]]
+   [:div.row [:a {:href "/about"} "go to about page"]]
+   [:div.row
+    [:div.col-md-4 [argument-of-issues-chart]]
+    [:div.col-md-4 [argument-of-issues-chart]]
+    [:div.col-md-4 [argument-of-issues-chart {:refresh-timeout 5000}]]]
+   [:div.row [:div.col-md-6 [argument-of-issues-chart]]
+    [:div.col-md-6 [argument-of-issues-chart]]]])
 
 (defn about-page []
   [:div [:h2 "About dbas-analytics"]
