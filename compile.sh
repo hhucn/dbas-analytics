77c8eef9bcf452f804748da51b9aa1de0bb5ad77
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2018-01-06 16:43:12 +0100

    Add auto deployment with circle-ci

---
+++ compile.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e # Exit with nonzero exit code if anything fails
+
+mkdir -p out
+
+echo "Compiling JS"
+lein package
+cp -R public/* out/
+rm -rf out/js/release
+
+# echo "Compiling sass"
+# sass --update out/css:out/css --style compressed
